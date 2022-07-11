f9aef7adab7aba10a07560b65a671e03a76d97ee
Zi-Shane <shane110319@gmail.com>
2022-07-11 12:43:29 +0800

    add split_sequence2()

--- ml_projets/split_sequence.py
+++ ml_projets/split_sequence.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import os
 
-def split_sequence(sequence: pd.DataFrame, timestep: int):
+def split_sequence2(sequence: pd.DataFrame, timestep: int) -> tuple[list[list[int]], list[int]]:
     X = list()
     y = list()
     for i in range(sequence.shape[0] - timestep - 1):
@@ -17,14 +17,24 @@ def split_sequence(sequence: pd.DataFrame, timestep: int):
     return X, y
 
 
-def split_sequence2(sequence: pd.DataFrame, timestep: int):
-    
-    return X, y
+def split_sequence(sequence: pd.DataFrame, timestep: int):
+    X = list()
+    y = list()
+    for i in range(len(sequence)):
+        end_idx = i + timestep
+        if end_idx > len(sequence) - 1:
+            break
+        seq_x, seq_y = sequence['Close'][i:end_idx].to_numpy(), sequence['Close'][end_idx]
+        X.append(seq_x)
+        y.append(seq_y)
+    return np.array(X), np.array(y)
 
 
 # %%
 data = pd.read_csv(os.getcwd() + '/datasets/BRK-A.csv')
-X, y = split_sequence(data[:10], 2)
-print(X, y)
+X, y = split_sequence2(data[:10], 2)
 
-print(data[:10])
+for a, b in zip(X, y):
+    print(a, b)
+
+# print(data['Close'][:10])
